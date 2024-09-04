6a89e4f34e1a59dcbe367b7b38b7a56648d83a86
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-04 14:53:41 +0200

    feat: create a children class and extend with Parent

--- Model/Food.php
+++ Model/Food.php
@@ -1,3 +1,15 @@
 <!-- classe figlia di Product -->
 
 <?php
+
+class Food extends Product {
+    public $size;
+    public $age;
+    public $foodType;
+
+    public function __construct(string $_name, float $_price, string $_img, Category $_category, ProductType $_type, $_foodType){
+        parent::__construct($_name, $_price, $_img, $_category, $_type);
+        $this->foodType = $_foodType;
+    }
+
+}
