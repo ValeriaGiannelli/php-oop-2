5fab4abf27db1b249d7d8a2b36ad511dbc2fbc7f
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-04 16:27:52 +0200

    feat: create class Game

--- Model/Game.php
+++ Model/Game.php
@@ -1,3 +1,12 @@
 <!-- classe figlia di Product -->
 
 <?php
+
+class Game extends Product {
+    private $gameType;
+
+    public function __construct(string $_name, float $_price, string $_img, string $_category, string $_type, string $_gameType){
+        parent::__construct($_name, $_price, $_img, $_category, $_type);
+        $this->gameType = $_gameType;
+    }
+}
