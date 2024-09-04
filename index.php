175c8dca3b6bdb4ebd189383f7e893c4f15efadc
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-09-04 14:44:14 +0200

    feat: add placeholder card

--- index.php
+++ index.php
@@ -14,9 +14,45 @@ require_once __DIR__.'/Model/ProductType.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Negozio animali Oop-2</title>
 </head>
 <body>
+
+    <!-- stampo in pagina le informazioni dei prodotti -->
+    <div class="d-flex">
+      
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="immagine del prodotto">
+                <div class="card-body">
+                    <!-- nome -->
+                    <h5 class="card-title">
+                        Nome prodotto
+                    </h5>
+
+                    <!-- prezzo -->
+                    <p class="card-text">
+                        prezzo del prodotto
+                    </p>
+
+                    <!-- tipologia -->
+                    <p class="card-text">
+                        tipologia di prodotto
+                    </p>
+
+                    <!-- categoria -->
+                    <p class="card-text">
+                        categoria animale
+                    </p>
+                    
+                    <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                </div>
+            </div>
+
+    </div>
     
 </body>
 </html>
